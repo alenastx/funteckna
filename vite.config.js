1a529e40970d4d93c758dcc53cc77b7345774044
Annalena Steinacher <e12220039@student.tuwien.ac.at>
2026-01-07 03:47:29 +0100

    added vite build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/funteckna/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/about_us.html'),
+        impressum: resolve(__dirname, 'src/impressum.html'),
+        datenschutz: resolve(__dirname, 'src/datenschutz.html'),
+        lizenz: resolve(__dirname, 'src/lizenz.html'),
+        barrierefreiheit: resolve(__dirname, 'src/barrierefreiheit.html'),
+      },
+    },
+  },
 })
